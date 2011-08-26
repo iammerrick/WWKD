ecd54d8bcf98202cacac1ac72fdd372ba1afe54e
Will Robertson <shoxty@gmail.com>
2011-08-26 14:28:02 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+<title>WWKD</title>
+<meta charset="UTF-8">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+   	<div class="container">
+		<div class="header">
+			<h1><img src="images/title.png" /></h1>
+		</div>
+		<div class="post">
+			I'm telling you man, this Android really made my life better. Food tastes better, even colors are brighter.
+	 	</div>
+		<div class="post">
+			You see, I think you are being too Christlike and I do not appreciate it. You need to be more like an Evangelical Christian and hate everyone thats different than you.
+	 	</div>
+		<div class="post">
+			I would love a crack addition because it would be AWESOME. Its a shame its so addictive.
+	 	</div>
+		<div class="post">
+			Ya, I've been screwed by the ORM before. I went home, got naked, got on Chatroulette and started cutting myself.
+	 	</div>
+		<div class="post">
+			Oh, I've seen that before. Thats definitely cancer.
+	 	</div>
+		<div class="post">
+			I don't like people with dreams.
+	 	</div>
+		<div class="post">
+			I'm pretty sure its called yiffing because of the sound a fox makes.
+	 	</div>
+		<div class="post">
+			Newborn babies are not good looking. I would NOT have emailed that photo.
+	 	</div>
+		<div class="post">
+			Just rack up all the debt you want. Spend all the money you want this year and hope on hyper inflation next year.
+	 	</div>
+		<div class="post">
+			That's not what Steinberger my Jewish lawyer said.
+	 	</div>
+		<div class="post">
+			I've done a pretty good job at making my wife feel like my roommate and not my wife.
+	 	</div>
+		<div class="post">
+			I have too many friends with birthdays. I need to get rid of my friends, they are a waste of time.
+	 	</div>
+		<div class="post">
+			I am just tired of Old Testament religions like Jews and Muslims.
+	 	</div>
+		<div class="post">
+			I'm gonna make you wear my balls as sunglasses.
+	 	</div>
+		<div class="post">
+			I don't believe in education but I do believe in taxes. I think you should tax everything.
+	 	</div>
+		<div class="post">
+			"How about you Kenneth, has Zen Coding helped you out like its helped me?" "Oh yeah, its even made my balls bigger."
+	 	</div>
+		<div class="post">
+			"Let's take Kenneth's car." "Unless you guys are up to date on your tetanus shots, that is a really bad idea."
+	 	</div>
+		<div class="post">
+			"Did you ever get the pine nut oil in the mail?" "Yes, but my wife found it first and I was too ashamed to tell her it was for weight loss."
+	 	</div>
+		<div class="post">
+			The Predator is serious business. If you have one red dot on your chest, run. If you have three red dots on your chest, you are already dead.
+	 	</div>
+		<div class="post">
+			If crapping out vegetable oil means those pills are working, than yes, those pills are working.
+	 	</div>
+	 </div>
+</body>
+</html>
